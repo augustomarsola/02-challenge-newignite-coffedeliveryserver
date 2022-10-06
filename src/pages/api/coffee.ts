4fb9ba9736ae66ad547c9df118cebe1d0000c057
Augusto Marsola <augusto.marsola@live.com>
2022-10-06 19:47:02 -0300

    Added cors

--- src/pages/api/coffee.ts
+++ src/pages/api/coffee.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
 
 type Data = {
   name: string;
@@ -114,9 +115,30 @@ const coffees = [
   },
 ];
 
-export default function handler(
+const cors = Cors({
+  methods: ["GET"],
+});
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  await runMiddleware(req, res, cors);
   res.status(200).json(coffees);
 }
